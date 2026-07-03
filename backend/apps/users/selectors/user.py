8fc195adcc1612057d17aa6f0b6f9c8483f9874c
Suwas Ghale <suwasghale693@gmail.com>
2026-07-03 10:08:17 +0545

    feat(users): add user selector

---
+++ backend/apps/users/selectors/user.py
@@ -0,0 +1,109 @@
+"""
+Selectors related to the User model.
+NOTE: Selectors are NOT services. They should not contain business logic only even not crud operations. They are responsible for reading data and returning it in a format that is useful for the caller.
+Selectors are responsible ONLY for reading data.
+They must never create, update or delete records.
+"""
+
+from __future__ import annotations
+
+from uuid import UUID
+
+from django.db.models import QuerySet
+
+from apps.users.models import User
+
+
+# ---------------------------------------------------------------------
+# Single Objects
+# ---------------------------------------------------------------------
+
+def get_user_by_id(user_id: UUID) -> User:
+    """
+    Return a user by UUID.
+
+    Raises:
+        User.DoesNotExist
+    """
+    return (
+        User.objects
+        .select_related("profile")
+        .get(pk=user_id)
+    )
+
+
+def find_user_by_email(email: str) -> User | None:
+    """
+    Return a user by email or None.
+    """
+    return (
+        User.objects
+        .select_related("profile")
+        .filter(email__iexact=email)
+        .first()
+    )
+
+
+# ---------------------------------------------------------------------
+# Lists
+# ---------------------------------------------------------------------
+
+def list_users() -> QuerySet[User]:
+    """
+    Return all users.
+    """
+    return (
+        User.objects
+        .select_related("profile")
+        .order_by("-created_at")
+    )
+
+
+def list_active_users() -> QuerySet[User]:
+    """
+    Return active users.
+    """
+    return (
+        User.objects
+        .select_related("profile")
+        .filter(is_active=True)
+        .order_by("-created_at")
+    )
+
+
+def list_verified_users() -> QuerySet[User]:
+    """
+    Return verified users.
+    """
+    return (
+        User.objects
+        .select_related("profile")
+        .filter(
+            is_active=True,
+            is_verified=True,
+        )
+        .order_by("-created_at")
+    )
+
+
+def list_staff_users() -> QuerySet[User]:
+    """
+    Return staff users.
+    """
+    return (
+        User.objects
+        .select_related("profile")
+        .filter(is_staff=True)
+        .order_by("email")
+    )
+
+
+# ---------------------------------------------------------------------
+# Boolean
+# ---------------------------------------------------------------------
+
+def exists_user_by_email(email: str) -> bool:
+    """
+    Check whether a user exists.
+    """
+    return User.objects.filter(email__iexact=email).exists()
